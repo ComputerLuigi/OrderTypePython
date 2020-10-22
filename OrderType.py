bdccdf5dbe12d02cdc9fc41933a66c72d0582fec
Joe Bernert <joebernert@gmail.com>
2020-10-22 19:01:53 +0100

    interactive input

--- OrderType.py
+++ OrderType.py
@@ -1,15 +1,16 @@
 c1 = (0,0)
 c2 = (1,1)
 c3 = (1,2)
+def OrientedDoubleArea(c1,c2,c3):
+	x1y2 = c1[0]*c2[1]
+	x1y3 = c1[0]*c3[1]
+	x2y3 = c2[0]*c3[1]
+	x2y1 = c2[0]*c1[1]
+	x3y1 = c3[0]*c1[1]
+	x3y2 = c3[0]*c2[1]
 
-x1y2 = c1[0]*c2[1]
-x1y3 = c1[0]*c3[1]
-x2y3 = c2[0]*c3[1]
-x2y1 = c2[0]*c1[1]
-x3y1 = c3[0]*c1[1]
-x3y2 = c3[0]*c2[1]
-
-OrientedDoubleArea =-1 * ( x1y2 - x1y3 + x2y3 - x2y1 + x3y1 - x3y2)  
+	ODA  =-1 * ( x1y2 - x1y3 + x2y3 - x2y1 + x3y1 - x3y2)
+	return ODA
 
 print(OrientedDoubleArea)
 
