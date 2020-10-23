8fccb51201bde777a34baa31b9d7b3e43cd9ba30
Joe Bernert <joebernert@gmail.com>
2020-10-23 02:32:22 +0100

    calling OrderType from the file handler works now

--- ReadInput.py
+++ ReadInput.py
@@ -6,4 +6,10 @@ with open( 'input' ) as f:
 		words = line.split(',')
 		b = (int(words[0]),int(words[1]))
 		a += tuple(b)
-print (a[2])
+import OrderType as ot
+c1 = (a[0],a[1])
+c2 = (a[2],a[3])
+c3 = (a[4],a[5])
+
+z = ot.OrientedDoubleArea(c1,c2,c3)
+print (z)
