321e8bc500f456b3da668e297f8d10812fc2c6fc
Joe Bernert <joebernert@gmail.com>
2020-10-23 19:57:49 +0100

    made coordinatelist, coordinatecombinations, and ordertypelist functions

--- ReadInput.py
+++ ReadInput.py
@@ -1,7 +1,6 @@
 import OrderType as ot
 import itertools as it
 
-a = []
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
@@ -9,17 +8,26 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
-with open( 'input' ) as f:
-	for line in f:
-		words = line.split(',')
-		b = (int(words[0]),int(words[1]))
-		a += (b)
+def makeCoordinateList():
+	a = []
+	with open( 'input' ) as f:
+		for line in f:
+			words = line.split(',')
+			b = (int(words[0]),int(words[1]))
+			a += (b)
 
-d = list (pairwise(a))
-d = d[0::2]
+	d = list (pairwise(a))
+	d = d[0::2]
+	return d
 
-e = list(it.combinations(d,3))
+def CoordinateCombinations(d):
+	e = (it.combinations(d,3))
+	return e
 
-for triple in e:
-	x = ot.OrientedDoubleArea(triple[0],triple[1],triple[2])
-	print (x)
+def ListOrderTypes(e):
+	x = []
+	for triple in iter(e):
+		x.append(ot.OrientedDoubleArea(triple[0],triple[1],triple[2]))
+	return x	
+
+print(ListOrderTypes(CoordinateCombinations(makeCoordinateList())))
