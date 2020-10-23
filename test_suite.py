ff1357382dbde20394c58b1b93781c951d3acd7c
Joe Bernert <joebernert@gmail.com>
2020-10-23 23:12:37 +0100

    Added test for coordinate packer

--- test_suite.py
+++ test_suite.py
@@ -14,3 +14,9 @@ def test_right_handed_triangle():
 	
 def test_left_handed_triangle():
 	assert ot.OrientedDoubleArea((0,0),(1,1),(2,3)) < 0
+
+def test_coordinate_packer():
+	a = [1,2,3,4,5,6]
+	import ReadInput as ri
+	assert ri.MakeCoordinateList(a) == [(1,2),(3,4),(5,6)]
+
