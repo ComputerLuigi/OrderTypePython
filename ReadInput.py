71651572562485c3cfa1900cc2366bd1e27882c8
Joe Bernert <joebernert@gmail.com>
2020-10-23 18:47:21 +0100

    removed dummy data from readinputs

--- ReadInput.py
+++ ReadInput.py
@@ -1,8 +1,8 @@
 import OrderType as ot
 import itertools as it
 
-
 a = []
+
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = it.tee(iterable)
@@ -10,18 +10,16 @@ def pairwise(iterable):
     return zip(a, b)
 
 with open( 'input' ) as f:
-
-
 	for line in f:
 		words = line.split(',')
 		b = (int(words[0]),int(words[1]))
 		a += (b)
-c1 = (a[0],a[1])
-c2 = (a[2],a[3])
-c3 = (a[4],a[5])
+
 d = list (pairwise(a))
 d = d[0::2]
+
 e = list(it.combinations(d,3))
+
 for triple in e:
 	x = ot.OrientedDoubleArea(triple[0],triple[1],triple[2])
 	print (x)
